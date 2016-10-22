0cd2739b9291023026f070b03c903d82d49851ad
Jeff Grady <jeff@dogearsoftware.com>
2016-10-22 19:02:32 -0400

    Test passes for fixed_xor()

--- set1.py
+++ set1.py
@@ -7,7 +7,7 @@ import base64
 import struct
 import unittest
 
-def hex_to_base64(hex_input):
+def hex_decode(hex_input):
     output = list()
     for i in xrange(0, len(hex_input), 2):
         number = 0
@@ -22,8 +22,28 @@ def hex_to_base64(hex_input):
         else:
             number += ord(char2.lower()) - ord('a') + 10
         output.append(number)
+    return output
+
+def hex_encode(string1):
+    output = u''
+    for i in xrange(0, len(string1)):
+        output += '%x' % (ord(string1[i]))
+    return output
+
+def hex_to_base64(hex_input):
+    output = hex_decode(hex_input)
     output = struct.pack('B' * len(output), *output)
     output = base64.b64encode(output)
+    return output
+
+def fixed_xor(string1, key):
+    string1 = hex_decode(string1)
+    key = hex_decode(key)
+    output = list()
+    for i in xrange(0, len(string1)):
+        output.append(string1[i] ^ key[i])
+    output = struct.pack('B' * len(output), *output)
+    output = hex_encode(output)
     return output
 
 class TestSet1(unittest.TestCase):
@@ -34,6 +54,13 @@ class TestSet1(unittest.TestCase):
         output = hex_to_base64(hex_input)
         expected_output = 'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc'
         expected_output += '29ub3VzIG11c2hyb29t'
+        self.assertEqual(output, expected_output)
+
+    def test_fixed_xor(self):
+        hex_input = '1c0111001f010100061a024b53535009181c'
+        key = '686974207468652062756c6c277320657965'
+        output = fixed_xor(hex_input, key)
+        expected_output = '746865206b696420646f6e277420706c6179'
         self.assertEqual(output, expected_output)
 
 if __name__ == '__main__':
